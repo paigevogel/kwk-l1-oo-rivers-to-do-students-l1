e8654269672381752970e5995aa93ec47bb989d8
paigevogel <paigevogel36@gmail.com>
2018-06-07 21:48:47 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -3,4 +3,7 @@ class River
   attr_accessor :name, :countries, :discharge
   
   def initialize(name, countries, discharge)
+    @name=name
+    @countries=countries 
+    @discharge=discharge
     
