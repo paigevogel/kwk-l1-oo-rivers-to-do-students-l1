fdba919f9fd38a2659f7b5405e28a903581d8750
paigevogel <paigevogel36@gmail.com>
2018-06-07 22:05:26 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -8,6 +8,11 @@ class River
     @countries=countries 
     @discharge=discharge
   end
+  
+  def flood 
+     @discharge= @discharge + @discharge*0.3
 end
 
 nile=River.new("Nile", 4258, "Tanzania, Uganda, Rwanda, Burundi, the Democratic Republic of the Congo, Kenya, Ethiopia, Eritrea, South Sudan, Sudan and Egypt", 99940)
+
+puts nile.discharge
